3a9837e8417499c6138ad990973fd73638e17ac3
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2022-05-11 16:06:08 +0530

    Update- Added rotes for authentication

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import NavigationBar from "./Components/Navigationbar/NavigationBar";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Mockman from "mockman-js";
+import Login from "./Pages/Authentication/Login";
+import Signup from "./Pages/Authentication/Signup";
 
 function App() {
   const { pathname } = useLocation();
@@ -11,12 +13,22 @@ function App() {
   return (
     <div className="App">
       <NavigationBar />
-      {pathname !== "/" && <Sidebar />}
+      {!(
+        pathname === "/" ||
+        pathname === "/login" ||
+        pathname === "/signup"
+      ) && <Sidebar />}
       <Routes>
         <Route path="/mockman" element={<Mockman />} />
       </Routes>
       <Routes>
         <Route path="/" element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   );
