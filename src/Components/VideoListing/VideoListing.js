8cab76d8767f4a6bf97252ebc81a9504d7d260fd
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2022-05-15 06:23:08 +0530

    Feat - display video lists

---
+++ src/Components/VideoListing/VideoListing.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./VideoListing.css";
+import VideoCard from "../VideoCard/VideoCard";
+import { useVideoContext } from "../../Hooks/useVideoContext";
+
+const VideoListing = () => {
+  const {
+    videoState: { videoList, category },
+  } = useVideoContext();
+
+  console.log(useVideoContext());
+  const sortByCategoryVideos =
+    category?.toLowerCase() === "all"
+      ? videoList
+      : videoList.filter(
+          (video) =>
+            video?.categoryName?.toLowerCase() === category?.toLowerCase()
+        );
+
+  console.log(sortByCategoryVideos);
+
+  return (
+    <div className="videoListing-section">
+      {sortByCategoryVideos?.length > 0 &&
+        sortByCategoryVideos.map((videoInfo) => (
+          <VideoCard videoInfo={videoInfo} key={`VideoCard-${videoInfo._id}`} />
+        ))}
+    </div>
+  );
+};
+
+export default VideoListing;
